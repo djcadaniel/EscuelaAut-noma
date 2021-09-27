154a4cba1b5fe5880ba7d8b0b9c20d7d77d21765
Daniel Castro <djcadaniel@gmail.com>
2021-09-27 18:38:43 -0500

    salio logica de opciones js

--- js/curso.js
+++ js/curso.js
@@ -1,21 +1,29 @@
-const itemCurso = document.querySelector('.cursos__contain--item')
-const pulsarCurso = itemCurso.addEventListener('click', a)
+// const itemCurso = document.querySelector('.cursos__contain--item')
+// const pulsarCurso = itemCurso.addEventListener('click', a)
 
-switch (pulsarCurso){
-        case itemCurso.querySelector('text1'):
-            console.log('hola1')
-            break
-        case itemCurso.querySelector('text2'):
-            console.log('hol2')
-            break
-        case itemCurso.querySelector('text3'):
-            console.log('hola3')
-            break
-        case itemCurso.querySelector('text4'):
-            console.log('hola4')
-            break
-        default:
-            console.log('hola5')
-            break
-    }
+const texto1 = document.querySelector('.cursos__contain--item1')
+const texto2= document.querySelector('.cursos__contain--item2')
+
+const mostrarTexto = () =>(
+    console.log('texto1')
+)
+texto1.addEventListener('click',mostrarTexto)
+
+// switch (pulsarCurso){
+//         case itemCurso.querySelector('text1'):
+//             console.log('hola1')
+//             break
+//         case itemCurso.querySelector('text2'):
+//             console.log('hol2')
+//             break
+//         case itemCurso.querySelector('text3'):
+//             console.log('hola3')
+//             break
+//         case itemCurso.querySelector('text4'):
+//             console.log('hola4')
+//             break
+//         default:
+//             console.log('hola5')
+//             break
+//     }
 
