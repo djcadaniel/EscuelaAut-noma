b4936cd8880aefbe12c770e5411dabb9d8fb223e
Daniel Castro <djcadaniel@gmail.com>
2021-09-30 20:40:17 -0500

    falta arrglar slide de nuevos cursos

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,22 @@
 const buttonMenu = document.querySelector('.header__button')
 const menu = document.querySelector('.header__menu')
+const menuItem1 = document.querySelector('.header__ul--li1')
+const menuItem2 = document.querySelector('.header__ul--li2')
+const menuItem3 = document.querySelector('.header__ul--li3')
+const menuItem4 = document.querySelector('.header__ul--li4')
+
 
 const ocultar = () =>(
     menu.classList.toggle('visible')
 )
 buttonMenu.addEventListener('click',ocultar)
+
+const mostrar = ()=>(
+    menu.classList.toggle('visible')
+)
+menuItem1.addEventListener('click', mostrar)
+menuItem2.addEventListener('click', mostrar)
+menuItem3.addEventListener('click', mostrar)
+menuItem4.addEventListener('click', mostrar)
+
+
